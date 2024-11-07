e35a3602a3e07596bf29f20d5d04e3e3eee43ebd
Harun Selçuk Çetin <harunselcukctn@gmail.com>
2024-11-07 17:10:25 +0300

    server sidebar
    server sidebar

---
+++ components/server/server-header.tsx
@@ -0,0 +1,96 @@
+"use client"
+import { MemberRole, Server } from "@prisma/client"
+import { ServerWithMembersWithProfiles } from "@/types"
+import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+
+
+interface ServerHeaderProps {
+    server: ServerWithMembersWithProfiles;
+    role?: MemberRole;
+}
+
+export const ServerHeader = ({
+    server,
+    role
+}: ServerHeaderProps) => {
+
+    const isAdmin = role === MemberRole.ADMIN;
+    const isModerator = isAdmin || role === MemberRole.MODERATOR;
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger
+                className="focus:outline-none"
+                asChild
+            >
+                <button className="w-full text-md font-semibold px-3 flex items-center h-12
+                border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 
+                dark:hover:bg:zinc-700/50 transition">
+                    {server.name}
+                    <ChevronDown className="h-5 w-5" />
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+                className="2-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]"
+            >
+                {
+                    isModerator && (
+                        <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+                            İnsanları Davet Et
+                            <UserPlus className="h-4 w-4 ml-auto" />
+                        </DropdownMenuItem>
+                    )
+                }
+                {
+                    isAdmin && (
+                        <DropdownMenuItem className=" px-3 py-2 text-sm cursor-pointer">
+                            Sunucu Ayarları
+                            <Settings className="h-4 w-4 ml-auto" />
+                        </DropdownMenuItem>
+                    )
+                }
+                {
+                    isAdmin && (
+                        <DropdownMenuItem className=" px-3 py-2 text-sm cursor-pointer">
+                            Kullanıcıları Yönet
+                            <Users className="h-4 w-4 ml-auto" />
+                        </DropdownMenuItem>
+                    )
+                }
+                {
+                    isModerator && (
+                        <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                            Kanal Oluştur
+                            <PlusCircle className="h-4 w-4 ml-auto" />
+                        </DropdownMenuItem>
+                    )
+                }
+                {isModerator && <DropdownMenuSeparator />}
+                {
+                    isAdmin && (
+                        <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                            Sunucuyu Sil
+                            <Trash className="h-4 w-4 ml-auto" />
+                        </DropdownMenuItem>
+                    )
+                }
+                {
+                    !isAdmin && (
+                        <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                            Sunucudan Ayrıl
+                            <LogOut className="h-4 w-4 ml-auto" />
+                        </DropdownMenuItem>
+                    )
+                }
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
---
+++ types.ts
@@ -0,0 +1,5 @@
+import { Server, Member, Profile } from "@prisma/client";
+
+export type ServerWithMembersWithProfiles = Server & {
+    memebers: (Member & { profile: Profile })[];
+}
